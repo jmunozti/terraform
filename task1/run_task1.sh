de39ddfba322bcded0d0e26a78e6e2e52df48f16
Jorge Muñoz <jmunozti@gmail.com>
2020-07-15 19:56:25 -0500

    Adding tfvars file

--- task1/run_task1.sh
+++ task1/run_task1.sh
@@ -3,11 +3,12 @@ echo "Begin"
 echo "============================================"
 
 sed -i "s/2020-terraform-state/$s3_bucket/g" main.tf
+sed -i "s/ssh-key/$key_pair/g" terraform.tfvars
 
 terraform fmt
 terraform init
 terraform plan
-terraform apply -input=false -auto-approve -var ssh_key="$key_pair"
+terraform apply -input=false -auto-approve
 
 echo "============================================"
 echo "End"
---
+++ task1/terraform.tfvars
@@ -0,0 +1,9 @@
+
+region              = "us-east-1"
+vpc_cidr            = "10.0.0.0/16"
+public_subnet_cidr  = "10.0.0.0/24"
+private_subnet_cidr = "10.0.1.0/24"
+ssh_key             = "ssh-key"
+instance_type       = "t2.micro"
+ami_id              = "ami-08f3d892de259504d"
+ec2_count           = "1"
